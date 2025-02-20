00ca3ff5e0b59ea4ec51bd92509d282e4e150a98
Am706870 <88397078+Am706870@users.noreply.github.com>
2025-02-20 15:24:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function submitBtn() {
+	let first = document.getElementById("first name").value;
+	let last = document.getElementById("last name").value;
+	let phone = document.getElementById("phone number").value;
+	let email = document.getElementById("email id").value;
+	alert("First Name:" +first +" "+ 
+		  "Last Name:"+last+" "+
+		  "Phone Number:"+phone +" "+ 
+		  "Email ID:"+email);
+	// document.getElementById("firstform").innerText = 
+
+}
